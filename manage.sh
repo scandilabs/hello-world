a948635482eb0b6a6f0c42172e7db05007cebe9a
Mads Kvalsvik <mkvalsvik@scandilabs.com>
2014-02-14 16:35:24 -0500

    Introduced environment-specific resources directories and pom.xml build profile changes see http://scandilabs.com/technology/knowledge/How_to_build_for_different_environments_with_Maven

--- manage.sh
+++ manage.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 
-# IMPORTANT: You must allow symlinks within your Tomcat webapps for this script to work.  
-# Edit TOMCAT_HOME/conf/context.xml and change <Context> to <Context allowLinking="true">
-
 # Your java project name. Defaults to current directory name if not set here.
 PROJECT_NAME=
 
 # Full path to your java project home location. Defaults to current directory if not set here.
 PROJECT_HOME=
 
-# Location of local exploded webapp. Defaults to [current dir]/target/[artifactId]-[version]
-# Note that a symlink will be created in the local tomcat webapps directory which will point back to this directory.
-LOCAL_WEBAPP_DIR=
+# Full path to your local Tomcat installation's home directory. 
+# This is the directory that contains bin, conf, lib, logs, webapps, work, etc.
+# Commonly used paths are: 
+#   /sl/apache-tomcat-7.0.34
+#   /catamaran/apache-tomcat-6.0.35
+#   /catamaran/servers/[project_name]/tomcat
+LOCAL_TOMCAT_HOME_DIR=/sl/apache-tomcat-7.0.34
 
-# Your local Tomcat /bin directory, where 'catalina.sh' lives. Defaults to /sl/apps/[PROJECT_HOME]/tomcat/bin
-LOCAL_TOMCAT_BIN_DIR=
-
-# Your local Tomcat /webapps directory, where the local webapp will be deployed (via a symlink to LOCAL_WEBAPP_DIR). 
-# Defaults to /sl/apps/[PROJECT_HOME]/tomcat/webapps
-LOCAL_TOMCAT_WEBAPPS_DIR=
+# Flag to make this script create a symlink inside Tomcat's webapps directory back to 
+# this project's src/main/webapp directory.  Set to 'false' if you are relying on Tomcat's 
+# context configuration files located in TOMCAT_HOME/conf/Catalina/localhost. 
+LOCAL_WEBAPP_CREATE_SYMLINK=true
 
 # The name of your webapp when it's deployed locally
 # Defaults to your maven pom.xml's [artifactId]-[version]. 
-# 'ROOT' is the right choice if you want your application deployed in the root context of tomcat (i.e. localhost/index.html).
+# 'ROOT' is the right choice if you want your application deployed in the root context of tomcat (i.e. accessible at http://localhost:8080/index.html).
 LOCAL_WEBAPP_NAME=ROOT
 
 # The name of your webapp when it's deployed to the server. 
 # Defaults to your maven pom.xml's [artifactId]-[version]. 
-# 'ROOT' is the right choice if you want your application deployed in the root context of tomcat (i.e. my.site.com/index.html).
+# 'ROOT' is the right choice if you want your application deployed in the root context of tomcat (i.e. accessible at http://my.site.com/index.html).
 REMOTE_WEBAPP_NAME=ROOT
 
 # Where the war file will be copied to on remote server
-# Defaults to LOCAL_TOMCAT_WEBAPPS_DIR
-# On Ubuntu, this is commonly /var/lib/tomcat7/webapps
-# IMPORTANT: Make both the user you're logging in with (i.e. 'sladmin') 
+# Defaults to /sl/apps/[PROJECT_HOME]/tomcat/webapps
+# On Amazon AWS Linux servers, this is usually /usr/share/tomcatX/webapps
+# On Ubuntu, this is usually /var/lib/tomcatX/webapps
+# IMPORTANT: Make sure both the user you're logging in with (i.e. 'ec2-user') 
 #            and the tomcat server user (i.e. 'tomcat7') can write to this directory
 REMOTE_TOMCAT_WEBAPPS_DIR=/var/lib/tomcat7/myapps
 
@@ -42,9 +42,11 @@ REMOTE_TOMCAT_WEBAPPS_DIR=/var/lib/tomcat7/myapps
 # DO NOT EDIT ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING
 # -----------------------------------------------------------------------
 
+ECHO_PREFIX='[manage.sh]'
+
 COMMAND=$1
 COMMAND_ARG_1=$2
-echo "[manage.sh] Running command '$COMMAND' .." 
+echo $ECHO_PREFIX "Running command '$COMMAND' .." 
 
 # initialize common variables
 CURRENT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -60,7 +62,7 @@ if [ -z "$PROJECT_HOME" ]; then
   PROJECT_HOME=$CURRENT_DIR
 fi  
 if [ -z "$LOCAL_WEBAPP_DIR" ]; then
-  LOCAL_WEBAPP_DIR=$PROJECT_HOME/target/$ARTIFACT_ID-$VERSION
+  LOCAL_WEBAPP_DIR=$PROJECT_HOME/src/main/webapp
 fi  
 
 # set local runtime app defaults
@@ -74,13 +76,8 @@ if [ ! -d $LOCAL_MY_APP_DIR ]; then
   mkdir -p $LOCAL_MY_APP_DIR
 fi
 
-# set tomcat home
-if [ -z "$LOCAL_TOMCAT_HOME_DIR" ]; then
-  LOCAL_TOMCAT_HOME_DIR=$LOCAL_MY_APP_DIR/tomcat
-fi
-
 if [ ! -d $LOCAL_TOMCAT_HOME_DIR ]; then
-  echo "Directory $LOCAL_TOMCAT_HOME_DIR was not found.  Install tomcat here, or create a symlink to an existing tomcat installation directory"
+  echo $ECHO_PREFIX "Directory $LOCAL_TOMCAT_HOME_DIR was not found.  Make sure you edit this script and add value for this varliable.  Then make sure Tomcat is installed in that location, or create a symlink to an existing tomcat installation directory"
   exit 1;
 fi
 
@@ -109,7 +106,7 @@ fi
 if [ "$COMMAND" == "start" ]; then
   cd $LOCAL_TOMCAT_BIN_DIR
   ./catalina.sh jpda start
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
@@ -117,127 +114,112 @@ fi
 if [ "$COMMAND" == "run" ]; then
   cd $LOCAL_TOMCAT_BIN_DIR
   ./catalina.sh run
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
 # branch on command input
 if [ "$COMMAND" == "rerun" ]; then
   $CURRENT_DIR/manage.sh build  
+  if [ $? != 0 ]; then
+    echo $ECHO_PREFIX 'EXITING, BUILD ERRORS'
+    exit 1;
+  fi
+
   $CURRENT_DIR/manage.sh run 
   
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
 if [ "$COMMAND" == "stop" ]; then
   cd $LOCAL_TOMCAT_BIN_DIR
   ./catalina.sh stop
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
 if [ "$COMMAND" == "build" ]; then
   cd $PROJECT_HOME
 
-  # Performs a maven webapp build BUT replaces static web content with symlinks to the source files
-  # so that local file changes appear immediately
-
-  # First remove old symlinks to static web content
-  if [ -f $LOCAL_WEBAPP_DIR ]; then
-    echo "[manage.sh] Removing /css, /js and /img symlinks from: $LOCAL_WEBAPP_DIR .."
-    rm $LOCAL_WEBAPP_DIR/WEB-INF/freemarker-www 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/css 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/js 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/img 2> /dev/null
-  fi  
-
-  # Then build which will move files from src/ to target/
-  echo "[manage.sh] Running 'mvn compile war:exploded' .."
-  mvn compile war:exploded
-  
-  # Remove newly copied static files from target/
-  echo "[manage.sh] Removing web files from: $LOCAL_WEBAPP_DIR .."
-  rm -r $LOCAL_WEBAPP_DIR/WEB-INF/freemarker-www 2> /dev/null
-  rm -r $LOCAL_WEBAPP_DIR/css 2> /dev/null
-  rm -r $LOCAL_WEBAPP_DIR/js 2> /dev/null
-  rm -r $LOCAL_WEBAPP_DIR/img 2> /dev/null
-
-  # Re-create the symlinks back to static files in the /src tree
-  echo "[manage.sh] Creating symlinks from /target to static web content under /src .."
-  cd $LOCAL_WEBAPP_DIR/WEB-INF/
-  ln -s $PROJECT_HOME/src/main/webapp/WEB-INF/freemarker-www/ freemarker-www
-  cd $LOCAL_WEBAPP_DIR
-  ln -s $PROJECT_HOME/src/main/webapp/css/ css
-  ln -s $PROJECT_HOME/src/main/webapp/js/ js
-  ln -s $PROJECT_HOME/src/main/webapp/img/ img
-
-  # Create a symlink in tomcat/webapps back to project's target directory
-  if [ ! -L $LOCAL_TOMCAT_WEBAPPS_DIR/$LOCAL_WEBAPP_NAME ]; then
-    echo "[manage.sh] Creating symlink from $LOCAL_TOMCAT_WEBAPPS_DIR/$LOCAL_WEBAPP_NAME to $LOCAL_WEBAPP_DIR .."
-    cd $LOCAL_TOMCAT_WEBAPPS_DIR
-    ln -s $LOCAL_WEBAPP_DIR $LOCAL_WEBAPP_NAME
-  else 
-    echo "[manage.sh] Webapp directory $LOCAL_TOMCAT_WEBAPPS_DIR/$LOCAL_WEBAPP_NAME exists already.  If your webapp doesn't start correctly, check that this directory points to the right webapp or delete it and this script will re-create it:"
-    WEBAPP_LS_OUTPUT="`ls -l $LOCAL_TOMCAT_WEBAPPS_DIR/$LOCAL_WEBAPP_NAME`"
-    echo "$WEBAPP_LS_OUTPUT"
+  # Performs a maven war:inplace build 
+  # so that local src file changes appear immediately
+  echo $ECHO_PREFIX "Starting 'mvn compile war:inplace' .."
+  mvn compile war:inplace
+  if [ $? != 0 ]; then
+    echo $ECHO_PREFIX 'EXITING, MAVEN ERRORS'
+    exit 1;
   fi
-  echo "[manage.sh] Finished command $COMMAND" 
+  
+  # Optionally create a symlink inside the local Tomcat/webapps directory back to the newly build webapp
+  if [ "$LOCAL_WEBAPP_CREATE_SYMLINK" == "true" ]; then
+    echo $ECHO_PREFIX "Creating symlink to src/main/webapp inside [$LOCAL_TOMCAT_WEBAPPS_DIR]"
+    cd $LOCAL_TOMCAT_WEBAPPS_DIR 
+
+    # First remove old symlink, if it exists
+    if [ -L $LOCAL_WEBAPP_NAME ]; then
+      echo $ECHO_PREFIX "Removing old symlink to [$LOCAL_WEBAPP_NAME] inside [$LOCAL_TOMCAT_WEBAPPS_DIR] .."
+      rm $LOCAL_WEBAPP_NAME
+    fi
+
+    # existing webapp directory exists inside tomcat?  if so rename it.
+    if [ -d $LOCAL_WEBAPP_NAME ]; then
+      DT=$(date +"%Y%m%d%H%M%S")
+      NEW_NAME=${LOCAL_WEBAPP_NAME}.${DT}
+      echo $ECHO_PREFIX "Renaming webapp [$LOCAL_WEBAPP_NAME] in [$LOCAL_TOMCAT_WEBAPPS_DIR] to [$NEW_NAME].."
+      mv $LOCAL_WEBAPP_NAME $NEW_NAME
+    fi
+      
+    # Then create new symlink
+    cd $LOCAL_TOMCAT_WEBAPPS_DIR
+    ln -s $LOCAL_WEBAPP_DIR $LOCAL_WEBAPP_NAME          
+  fi
+  
+  echo $ECHO_PREFIX "Finished command $COMMAND"
   exit 0  
 fi
 
 if [ "$COMMAND" == "clean" ]; then
-  cd $PROJECT_HOME
 
-  # First remove old symlinks
-  if [ -f $LOCAL_WEBAPP_DIR ]; then
-    echo "[manage.sh] Removing symlinks from: $LOCAL_WEBAPP_DIR .."
-    rm $LOCAL_WEBAPP_DIR/WEB-INF/freemarker-www 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/css 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/js 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/img 2> /dev/null
-  fi  
-  
-  # Remove symlink in tomcat/webapps back to project's target directory
-  echo "[manage.sh] Removing symlink or directory $LOCAL_WEBAPP_NAME inside $LOCAL_TOMCAT_WEBAPPS_DIR .."
-  cd $LOCAL_TOMCAT_WEBAPPS_DIR
-  rm $LOCAL_WEBAPP_NAME
-  
-  # Then maven clean
-  echo "[manage.sh] Performing 'mvn clean' in $PROJECT_HOME .."
+  # maven clean
+  echo $ECHO_PREFIX "Performing 'mvn clean' in $PROJECT_HOME .."
   cd $PROJECT_HOME
   mvn clean
 
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
 if [ "$COMMAND" == "restart" ]; then
   $CURRENT_DIR/manage.sh stop
   $CURRENT_DIR/manage.sh build
+  if [ $? != 0 ]; then
+    echo $ECHO_PREFIX 'EXITING, BUILD ERRORS'
+    exit 1;
+  fi
   $CURRENT_DIR/manage.sh start
 
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND"
   exit 0  
 fi
 
 if [ "$COMMAND" == "status" ]; then
   STATUS=`ps -ef | grep $LOCAL_TOMCAT_BIN_DIR | grep -v "grep"`
   # TODO output something if empty
-  echo "[manage.sh] Local tomcat 'ps -ef' status output:"
+  echo $ECHO_PREFIX "Local tomcat 'ps -ef' status output:"
   echo "$STATUS"
 
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND"
   exit 0  
 fi
 
 if [ "$COMMAND" == "status-all" ]; then
   STATUS=`ps -ef | grep "java" | grep -v "grep"`
   # TODO output something if empty
-  echo "[manage.sh] Local java 'ps -ef' status output:"
+  echo $ECHO_PREFIX "Local java 'ps -ef' status output:"
   echo "$STATUS"
 
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
@@ -245,31 +227,26 @@ if [ "$COMMAND" == "deploy" ]; then
 
   # Validate command line parameters
   if [ -z "$COMMAND_ARG_1" ]; then
-    echo "[manage.sh] Please specify target server hostname (as you would specify a ssh/scp host)"
+    echo $ECHO_PREFIX "Please specify target server hostname (as you would specify a ssh/scp host)"
     exit 1
   fi  
 
   # Stop local server if running
   $CURRENT_DIR/manage.sh stop
 
-  # First remove old symlinks
-  if [ -f $LOCAL_WEBAPP_DIR ]; then
-    echo "[manage.sh] Removing symlinks from: $LOCAL_WEBAPP_DIR .."
-    rm $LOCAL_WEBAPP_DIR/WEB-INF/freemarker-www 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/css 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/js 2> /dev/null
-    rm $LOCAL_WEBAPP_DIR/img 2> /dev/null
-  fi  
-
   # Then build a war
-  echo "[manage.sh] Running 'mvn compile war:war' .."
+  echo ECHO_PREFIX "Running 'mvn compile war:war' .."
   mvn compile war:war
+  if [ $? != 0 ]; then
+    echo $ECHO_PREFIX 'EXITING, MAVEN ERRORS'
+    exit 1;
+  fi
 
   # Copy to remote
-  echo "[manage.sh] Copying war file to $COMMAND_ARG_1"
+  echo $ECHO_PREFIX " Copying war file to $COMMAND_ARG_1"
   scp target/$ARTIFACT_ID-$VERSION.war $COMMAND_ARG_1:$REMOTE_TOMCAT_WEBAPPS_DIR/$REMOTE_WEBAPP_NAME.war
   
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
@@ -277,17 +254,17 @@ if [ "$COMMAND" == "install-jar" ]; then
 
   # Validate command line parameters
   if [ -z "$COMMAND_ARG_1" ]; then
-    echo "[manage.sh] Please specify jar file to install into local maven repository)"
+    echo $ECHO_PREFIX "Please specify jar file to install into local maven repository)"
     exit 1
   fi  
 
   # Then build a war
-  echo "[manage.sh] Running 'mvn install:install-file ' .."
+  echo $ECHO_PREFIX "Running 'mvn install:install-file ' .."
   mvn install:install-file -D
   # finish this later: http://ianibbo.blogspot.com/2009/04/google-apis-maven2-artifacts.html
   # and http://javastack.blogspot.com/2009/11/adding-jar-to-local-file-system-maven2.html
 
-  echo "[manage.sh] Finished command $COMMAND" 
+  echo $ECHO_PREFIX "Finished command $COMMAND" 
   exit 0  
 fi
 
